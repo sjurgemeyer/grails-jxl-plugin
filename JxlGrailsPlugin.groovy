7bb4cddc40af22ddad507b9f8dffe4b00338f3c9
Shaun Jurgemeyer <sjurgemeyer@gmail.com>
2012-02-24 23:47:05 -0600

    Updating plugin version number

--- JxlGrailsPlugin.groovy
+++ JxlGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class JxlGrailsPlugin {
     // the plugin version
-    def version = "0.5"
+    def version = "0.51"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.6 > *"
     // the other plugins this plugin depends on
